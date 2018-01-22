a9095e063424bb839121bc9b8f93bc7d4bf5cbcf
ratsjosh <ratsjosh@outlook.com>
2018-01-22 23:31:20 +0800

    Add scroll effect

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ const router = express.Router()
 router.get('/', (req, res) => {
   const database = db.get(process.env.DB_COLLECTION)
   database.collection('quotes').find().toArray((err, docs) => {
-    res.render('index', { quotes: docs, title: 'Yap Wei Jie Joshua' })
+    res.render('index', {
+      quotes: docs,
+      title: 'Yap Wei Jie Joshua',
+      image1: 'https://s3-ap-southeast-1.amazonaws.com/nccmain/Landing/YOURE%20HOME/welcomecup08dec2017.jpg',
+      image2: 'https://s3-ap-southeast-1.amazonaws.com/nccmain/Landing/NCTV05_20171116065338.jpg',
+    })
   })
 })
 
